cbd502f2aad8a7a27920ea0468bacaa475b0f731
Bennie Draaijer <benniedraaijer@users.noreply.github.com>
2020-05-05 09:36:44 +0200

    -

---
+++ WEEK_1/W1D2-1/script.js
@@ -0,0 +1,20 @@
+console.log("Hello Winc Academy");
+
+let myName = "Bennie Draaijer";
+console.log(myName);
+
+let number1 = 20;
+let number2 = 12;
+console.log(number1 + number2);
+
+console.log(3256 + 1547);
+console.log(20 - 6547);
+console.log(0.2358 * 0.2457);
+console.log(1.5 / 18);
+console.log(15 % 200);
+
+let myAge = 34;
+console.log(typeof myAge);
+
+myAge = "vierendertig";
+console.log(typeof myAge);
