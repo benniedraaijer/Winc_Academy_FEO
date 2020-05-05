9c3e641a6f273538cfcb4836cc6d954f7c9bb9f1
Bennie Draaijer <benniedraaijer@users.noreply.github.com>
2020-05-05 14:03:00 +0200

    W1D2-4

---
+++ WEEK_1/W1D2-4/script.js
@@ -0,0 +1,49 @@
+// Bezoeker 1
+const age_1 = 34;
+const isFemale_1 = false;
+const driverStatus_1 = 'bob';
+
+if (age_1 >= 18) {
+    console.log('Bezoeker 1 is 18 jaar of ouder, en mag naar binnen');
+
+} else {
+    console.log('Bezoeker 1 is jonger dan 18 jaar en mag daarom niet naar binnen');
+}
+
+if (isFemale_1) {
+    console.log('Bezoeker 1 is een vrouw en mag een ticket bestellen voor de Ladys Night ');
+} else {
+    console.log('Bezoeker 1 is geen vrouw en mag geen ticket bestellen voor de Ladys Night');
+}
+
+if (driverStatus_1 === 'bob') {
+    console.log('Bestuurder 1 is de BOB en mag daarom rijden ');
+
+} else {
+    console.log('Bestuurder 2 is geen BOB en mag daarom niet rijden ');
+}
+
+// Bezoeker 2
+const age_2 = 12;
+const isFemale_2 = true;
+const driverStatus_2 = 'annie';
+
+if (age_2 >= 18) {
+    console.log('Bezoeker 2 is 18 jaar of ouder, en mag naar binnen');
+
+} else {
+    console.log('Bezoeker 2 is jonger dan 18 jaar en mag daarom niet naar binnen');
+}
+
+if (isFemale_2) {
+    console.log('Bezoeker 2 is een vrouw en mag een ticket bestellen voor de Ladys Night ');
+} else {
+    console.log('Bezoeker 2 is geen vrouw en mag geen ticket bestellen voor de Ladys Night');
+}
+
+if (driverStatus_2 === 'bob') {
+    console.log('Bestuurder 2 is de BOB en mag daarom rijden ');
+
+} else {
+    console.log('Bestuurder 2 is geen BOB en mag daarom niet rijden ');
+}
